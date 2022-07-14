47e620258cebbf25947b73173762c41f4ae08cd0
tta2yta <tta2yta@gmail.com>
2022-07-14 15:41:20 +0300

    config basic express parameters

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("kkk");
+import express from "express";
+import config from "../config/defaults";
+// import { deserializerUser } from "./middleware";
+
+const port = config.port as number;
+const host = config.host as string;
+
+console.log(port);
+
+const app = express();
+// app.use(deserializerUser);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+  console.log(`server listening at http://${host}:${port}`);
+});
