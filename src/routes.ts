fbcd80714aeadc287adb8bb11342cd42b60585c0
tta2yta <tta2yta@gmail.com>
2022-07-15 15:38:06 +0300

    add route to register user

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controllers/user.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  //Register user
+  app.post("/api/usres", validateRequest(createUserSchema), createUserHandler);
 }
