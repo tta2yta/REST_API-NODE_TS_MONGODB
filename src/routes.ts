9c036839b356de648685bb6a1bda883d97dc3a39
tta2yta <tta2yta@gmail.com>
2022-07-16 20:45:00 +0300

    add create session route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,22 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controllers/user.controller";
-import { createUserSchema } from "./schema/user.schema";
+import {
+  createUserSchema,
+  createUserSessionSchema,
+} from "./schema/user.schema";
 import validateRequest from "./middleware/validateRequest";
+import { createUserSessionHandler } from "./controllers/session.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   //Register user
   app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
+
+  //login
+  app.post(
+    "api/session/",
+    validateRequest(createUserSessionSchema),
+    createUserSessionHandler
+  );
 }
